ba701db67f02f5c404aa20d9267b7ebb1c449560
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-10-06 15:33:55 +0200

    new build

--- app/scripts/controllers/politicians_controller.js
+++ app/scripts/controllers/politicians_controller.js
@@ -1,3 +1,16 @@
 Noselus.PoliticiansController = Ember.ArrayController.extend({
+  politicianFromWal: function() {
+    var politicians = this.filter(function(politician) {
+      return politician.get('assembly') === 'WAL';
+    });
+    return politicians;
+  }.property('@each.assembly'),
+
+  politicianFromFed: function() {
+    var politicians = this.filter(function(politician) {
+      return politician.get('assembly') === 'FED';
+    });
+    return politicians;
+  }.property('@each.duration')
 });
 
