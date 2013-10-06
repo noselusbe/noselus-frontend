5e4f77d6575e7815036e25116a807c22e64edaf4
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-10-06 11:15:34 +0200

    Added scroll to when transition to these routes

--- app/scripts/views/politician_view.js
+++ app/scripts/views/politician_view.js
@@ -1,7 +1,7 @@
 Noselus.PoliticianView = Ember.View.extend({
-    templateName: 'politician',
-    classNames: ['row'],
-    didInsertElement: function (argument) {
-      $('body').animate({scrollTop: 0}, 'fast'); // Scroll top
-    }
+  templateName: 'politician',
+  classNames: ['row'],
+  didInsertElement: function (argument) {
+    $('body').animate({scrollTop: 0}, 'fast'); // Scroll top
+  }
 });
--- app/scripts/views/question_view.js
+++ app/scripts/views/question_view.js
@@ -1,2 +1,5 @@
 Noselus.QuestionView = Ember.View.extend({
+  didInsertElement: function (argument) {
+    $('body').animate({scrollTop: 0}, 'fast'); // Scroll top
+  }
 });
