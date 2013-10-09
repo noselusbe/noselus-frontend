63b99c0a1d9809ff9f35fdb11ae91fcc338fc0d7
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-10-09 14:57:51 +0200

    new build page homepage

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -1,8 +1,3 @@
-Noselus.ApplicationRoute = Ember.Route.extend({
-});
+Noselus.ApplicationRoute = Ember.Route.extend();
 
-Noselus.IndexRoute = Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('questions');
-  }
-});
+Noselus.IndexRoute = Ember.Route.extend();
