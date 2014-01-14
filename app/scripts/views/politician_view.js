158d5700b4fd5ab40bb6bc3c5839a280dc47d948
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-01-14 22:48:05 +0100

    More refactoring and some design tweaks

--- app/scripts/views/politician_view.js
+++ app/scripts/views/politician_view.js
@@ -1,6 +1,6 @@
 Noselus.PoliticianView = Ember.View.extend({
   templateName: 'politician',
-  classNames: ['container'],
+  classNames: ['container main-wrapper'],
   didInsertElement: function (argument) {
     $('body').animate({scrollTop: 0}, 'fast'); // Scroll top
   }
--- app/scripts/views/question_view.js
+++ app/scripts/views/question_view.js
@@ -1,5 +1,5 @@
 Noselus.QuestionView = Ember.View.extend({
-  classNames: ['container'],
+  classNames: ['container main-wrapper'],
   didInsertElement: function (argument) {
     $('body').animate({scrollTop: 0}, 'fast'); // Scroll top
   }
