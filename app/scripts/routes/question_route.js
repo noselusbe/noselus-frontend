7598e5cf77fa78f76adcfa5a2c3de7656a80bb5a
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-01-19 22:53:09 +0100

    Make the "Retour aux questions" button origin aware Fixes #12

--- app/scripts/routes/question_route.js
+++ app/scripts/routes/question_route.js
@@ -1,5 +1,15 @@
 Noselus.QuestionRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('question', params.question_id);
+  },
+
+  actions: {
+    goBack: function (politician) {
+      if (Noselus.previousPath == 'questions') {
+        this.transitionTo('questions');
+      } else {
+        this.transitionTo('politician', politician);
+      }
+    }
   }
 });
