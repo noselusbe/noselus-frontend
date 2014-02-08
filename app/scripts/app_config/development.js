081a639b59280a11d476ad64771528185f25bd69
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-02-08 23:56:27 +0100

    fixes new backend url for development environment #19

--- app/scripts/app_config/development.js
+++ app/scripts/app_config/development.js
@@ -7,7 +7,7 @@ var AppConfig = {
 
   // Clone of production + new structure
   // ===================================
-  apiAdapterUrl: 'http://hurion.eu:4567',
+  apiAdapterUrl: 'http://noselus.hurion.eu',
   mixpanel: {
     api_key: '7ba8702ce621c550311efeb2d0f2a620'
   }
--- app/scripts/app_config/production.js
+++ app/scripts/app_config/production.js
@@ -1,6 +1,6 @@
 var AppConfig = {
   env: 'production',
-  apiAdapterUrl: 'http://hurion.eu:4567',
+  apiAdapterUrl: 'http://noselus.hurion.eu',
   mixpanel: {
     api_key: '7ba8702ce621c550311efeb2d0f2a620'
   }
