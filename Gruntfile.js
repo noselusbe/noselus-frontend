a19c602383e861362c4de905ddc7373a99097413
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-08-01 22:54:27 +0200

    wip deploy task

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*globals module*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    exec: {
+      ember_build: {
+        command: 'ember build'
+      }
+    },
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:noselusbe/noselus-frontend.git',
+          branch: 'gh-pages'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-build-control');
+
+  grunt.registerTask('deploy', ['exec:ember_build', 'buildcontrol']);
+};
