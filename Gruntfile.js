5045c0b4d6d72f48e3901684949b4e7eb6662958
Boris Rorsvort <boris.rorsvort@gmail.com>
2014-09-07 17:36:51 +0200

    fixes deploy file

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         command: 'ember build --environment=production'
       },
       release: {
-        command: 'git checkout master; git merge master;'
+        command: 'git checkout master; git merge develop;'
       },
       back_to_dev: {
         command: 'git checkout develop;'
@@ -31,5 +31,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-build-control');
 
-  grunt.registerTask('deploy', ['exec:ember_build', 'buildcontrol', 'exec:release', 'exec:back_to_dev']);
+  grunt.registerTask('deploy', ['exec:release', 'exec:ember_build', 'buildcontrol', 'exec:back_to_dev']);
 };
